16117a167a24c458fe23fe987c09b053dad8574d
BowlOfLoki <73730310+BowlOfLoki@users.noreply.github.com>
2022-10-13 20:39:11 +1100

    updates

--- script.js
+++ script.js
@@ -1,68 +1,137 @@
 /* Main functions loading most functions - */
 	/* Start - split into different functions like original python scripts*/
-function onLoadEvent() {
-	return null;
+
+
+
+
+
+twabIds = {};
+
+async function onLoadEvent() {
+	await twabFinder().then(function(response) {
+		twabIds = response;
+	})
+	await htmlOuter(twabIds).then(function(twabs) {
+		twabIds = twabs;
+		console.log('done')
+	})
+
+	let elements = document.querySelectorAll(".searchForm");
+	for (let i=0; i<elements.length; i++){
+		elements[i].style.visibility = "visible";
+	}
+
+	removeElement("loader")
+
+	let form = document.getElementById("searchform");
+	form.addEventListener('submit', formSubmit)
 }
 
-/* function for testing anything I need */
-function testFunc() {
-	var array = [1,2,3,4];
-	for (let x in array) {
-		console.log(array[x])
-	}
-	console.log("pressed")
-	twabFinder()
-	return null;
+function formSubmit(event) {
+	event.preventDefault();
+	console.log(document.getElementById("searchTerm").value);
+	return false;
 }
+
+
+
 /* General Use functions */
 	/* Used for all api requests so no repeated code is used */
 function apiRequest(url) {
-	var apiKey = "f964b21004ec4b688c3bfd113638b260";
+	let apiKey = "f964b21004ec4b688c3bfd113638b260";
+	let request = new XMLHttpRequest();
 
-	var request = new XMLHttpRequest();
-	request.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+	request.open("GET", url, true);
 	request.setRequestHeader("X-API-Key", apiKey);
-
-	request.onreadystatechange = function(){
-		if(this.readyState === 4 && this.status === 200){
-			var json = JSON.parse(this.responseText);
-			console.log(JSON.stringify(json));
+	return new Promise(function(resolve, reject) {
+		request.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				let json = JSON.parse(this.responseText);
+				resolve(json)
+			} else {
+				resolve.onerror = reject;
+			}
 		}
-	}
-	request.send();
+		request.send();
+	});
 }
+
+function removeElement(className) {
+	const elements = document.getElementsByClassName(className);
+	while(elements.length > 0) {
+		elements[0].parentNode.removeChild(elements[0])
+	}
+}
+
+function hideHtml(id, state) {
+	if (state) {
+		document.getElementById(id).style.visibility = "hidden";
+	} else {
+		document.getElementById(id).style.visibility = "visible";
+	}
+
+}
+
 	/* Clears text of characters that will ruin formatting when used in html */
 function sanitiseText(text) {
-	let result = text.replace("\xa0","").replace("\n"," ").replace("<div>","").replace("</div>","").replace("<p>","").replace("</p>","").replace("<ul>","").replace("</ul>","");
-	console.log(result);
-	return result;
+	return text.replace("\xa0","").replace("\n"," ").replace("<div>","").replace("</div>","").replace("<p>","").replace("</p>","").replace("<ul>","").replace("</ul>","");
 }
 
 
-function fileExist(url) {
-	if (url) {
-		var req = new XMLHttpRequest();
-		req.open('GET', url, false);
-		req.send();
-		return req.status==200;
-	} else {
-		return false;
-	}
-}
 
 /* Main Functions */
-	/* twab finder - finds twab ids*/
-function twabFinder(){
-	let page = 0;
-	let entries = [];
-	var cont = true;
-	var array = ["News", "Updates"];
-	for (let type in array) {
-		console.log(array[type])
-		while (cont === true) {
-			let url = "https://www.bungie.net/Platform/Trending/Categories/{type}/{page}/".replace("{page}",page).replace("{type}",array[type])
-			let data = apiRequest(url);
-			break;
+	/* twab finder - finds twab ids {id: [name, date]}*/
+async function twabFinder(){
+	entries = {};
+	contType = ["News", "Updates"];
+	for (let type in contType) {
+		let page = 0;
+		continueLoop = true;
+		while (true) {
+			let url = "https://www.bungie.net/Platform/Trending/Categories/{type}/{page}/".replace("{page}",page).replace("{type}",contType[type])
+			await apiRequest(url).then(function(response) {
+				let resultList = response.Response.results;
+				for (let i in resultList) {
+					let id = resultList[i].identifier
+					if (id in entries) {
+					} else if (!(resultList[i].displayName.toLowerCase().includes("this week at bungie")) &&  resultList[i].displayName.toLowerCase().includes("destiny 2") && contType[type] === "Updates") {
+						entries[id] = [resultList[i].displayName, resultList[i].creationDate];
+					} else if (resultList[i].displayName.toLowerCase().includes("this week at bungie") && contType[type] === "News") {
+						entries[id] = [resultList[i].displayName, resultList[i].creationDate.substring(0,10)];
+					}
+					if (response.Response.hasMore === false) {
+						continueLoop = false;
+					}
+				}
+			});
+			page += 1;
+			if (continueLoop === false) {
+				break;
+			}
 		}
 	}
+	return entries;
+}
+
+
+async function htmlScrape(id){
+	htmlObj = document.createElement('div');
+	let url = "https://www.bungie.net/Platform/Trending/Details/News/{id}".replace("{id}", id);
+	await apiRequest(url).then(function(response) {
+		let content = response.Response.news.article.properties.Content;
+		htmlObj.innerHTML = content;
+		removeElement("twitter-tweet");
+	})
+	return htmlObj;
+}
+
+async function htmlOuter(twabIds) {
+	number = 0;
+	for (let id in twabIds) {
+		htmlScrape(id).then(function(response) {
+			twabIds[id].push(response);
+			number += 1;
+		})
+	}
+	return twabIds;
 }
