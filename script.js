a8234b3048e8d30d14e8cc61504c741f24ded2ad
BowlOfLoki <73730310+BowlOfLoki@users.noreply.github.com>
2022-10-14 15:15:19 +1100

    went back to what worked (showing api key)

--- script.js
+++ script.js
@@ -6,10 +6,8 @@
 
 
 twabIds = {};
-apiKey = "";
 
 async function onLoadEvent() {
-	apiKey =
 	await twabFinder().then(function(response) {
 		twabIds = response;
 	})
@@ -97,7 +95,7 @@ async function formSubmit(event) {
 /* General Use functions */
 	/* Used for all api requests so no repeated code is used */
 function apiRequest(url) {
-
+	let apiKey = "f964b21004ec4b688c3bfd113638b260";
 	let request = new XMLHttpRequest();
 
 	request.open("GET", url, true);
@@ -113,22 +111,6 @@ function apiRequest(url) {
 		}
 		request.send();
 	});
-}
-
-async function netlifyKey() {
-	apiKey = "";
-	let request = new XMLHttpRequest();
-	request.open("GET", "https://destiny2twabs.netlify.app/.netlify/functions/keys")
-	return new Promise(function(response) {
-		request.onreadystatechange = function () {
-			if (this.readyState === 4 && this.status === 200) {
-				let json = JSON.parse(this.responseText);
-				apiKey = this.responseText;
-			}
-		}
-		request.send()
-	});
-	return apiKey;
 }
 
 
